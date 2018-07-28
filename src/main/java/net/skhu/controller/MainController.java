c99893fce58e66849ca7b78ae7dd0076a777e6fb
wkdtndgns <wkdtndgns@naver.com>
2018-07-29 01:19:50 +0900

    post 하면 퍼센트  return

--- src/main/java/net/skhu/controller/MainController.java
+++ src/main/java/net/skhu/controller/MainController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import net.skhu.domain.rank;
 import net.skhu.repository.RankRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("api")
 public class MainController {
 
@@ -19,18 +21,39 @@ public class MainController {
 	
     @RequestMapping("list")
     public List<rank> list(Model model) {
-        List<rank> list = rank.findAll();
+    	
+        List<rank> list = rank.findAllByOrderByPayDesc();
         model.addAttribute("list", list);
   
         return list;
     }
     
     @RequestMapping("insert/{pay}")
-    public void rank(@PathVariable("pay") int pay) {
+    public double rank(@PathVariable("pay") int pay) {
     	rank r = new rank(); 
     	r.setPay(pay);
     	rank.save(r);
  	
+    	List<rank> list = rank.findAllByOrderByPayDesc();
+    	
+    	
+    	int size = list.size();
+    	double result=0;
+    	int index = 0;
+    	
+    	for(int i=0; i<size; i++) {
+    		if(list.get(i).getPay()==pay) {
+    			index=i;
+    		}    		
+    	}
+    	
+    	System.out.println(size +" "+ index);
+    	
+    	System.out.format("%.2f%n", index/(double)size);
+    	result=index/(double)size;
+    	System.out.println(result);
+    	
+    	return result;
     }
 
 
--- src/main/java/net/skhu/repository/RankRepository.java
+++ src/main/java/net/skhu/repository/RankRepository.java
@@ -1,9 +1,11 @@
 package net.skhu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.skhu.domain.rank;
 
 public interface RankRepository extends JpaRepository<rank, Integer> {
-
+	List<rank> findAllByOrderByPayDesc();
 }
